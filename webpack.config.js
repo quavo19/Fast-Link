5fb91a6823bee9cebfbb5fbc92c09dbfcbe31beb
AkiteDonald <donaldakite27@gmail.com>
2022-12-16 23:43:21 +0000

    Complete Configuration for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,27 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  mode: 'development',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-      template: './src/index.html',
-    }),
-  ],
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
+  //   optimization: {
+  //     runtimeChunk: 'single',
+  //   },
 };
