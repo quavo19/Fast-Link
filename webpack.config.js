a47c57650d6563dc266e102ebef2b97145287f56
AkiteDonald <donaldakite27@gmail.com>
2022-12-16 23:30:50 +0000

    configure webpack for css and html

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
